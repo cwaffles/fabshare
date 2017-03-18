aa0f1e120032232442232182c92647eef16cf16a
cwaffles <cwaffles@users.noreply.github.com>
2017-03-18 16:38:45 -0700

    able to get stuff

--- rh.py
+++ rh.py
@@ -1,13 +1,17 @@
 import requests
 import subprocess
 
-nodeURL = 'http://localhost'
-nodePort = 4321
+#server refers to mojio's api
+apiURL = 'https://api.moj.io/v2/'
+authToken = 'Bearer 70c22344-e864-45e1-be1c-8b20db5cfb39'
 
 def getTrips():
-    address = nodeURL + ':' + str(nodePort) + '/'
-    print(address)
-    return requests.get(address).content
+    endpoint = 'trips'
+    headers = {'Authorization': authToken}
+    reqUrl = apiURL + endpoint
+    print(reqUrl)
+    return requests.get(reqUrl, headers=headers).content
+
 
 
 def init():
