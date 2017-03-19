2b1ed5123e3e90190b7c41d89a728c3592494b8b
cwaffles <cwaffles@users.noreply.github.com>
2017-03-19 00:50:39 -0700

    almost finished sql insertion

--- homePageController.py
+++ homePageController.py
@@ -9,40 +9,87 @@ def index():
     # getTrips()
     return "<html><title></title><body><p>Hello World!</p></body></html>"
 
-#Loads summary trip info from Mojio, returns 2d array (list) with summary data by trip
-def getFilteredTrips():
-    tripsJSON = getUnfilteredTrips()
-    # print(tripsJSON)
-    allRelData = [] #list for final storage of all relevant data
-    for trip in tripsJSON["Data"]:
-        #Trip ID // Start City // End City // Start Timestamp // Distance[m] // Fuel Efficiency[km/L]// Vehicle ID
-        relevantDataList = {"tid":              trip["Id"],
-                            "startCity":        trip["StartLocation"]["Address"]["City"],
-                            "endCity":          trip["EndLocation"]["Address"]["City"],
-                            "startTimestamp":   trip["StartTimestamp"],
-                            "distance":         trip["Distance"]["Value"],
-                            "efficiency":       trip["FuelEfficiency"]["Value"],
-                            "vid":              trip["VehicleId"]}
-        allRelData.append(relevantDataList)
-    return allRelData
+def init():
+    #do all the data loading here
+    loadTripsIntoDB("test@test.com")
+    return #stub
 
+#helper function to make http request
 def getFromAPI(url):
     headers = {'Authorization': globals.authToken}
-    # print(url)
     return requests.get(url, headers=headers).content
 
+#returns raw unfiltered trips
 def getUnfilteredTrips():
     endpoint = 'trips'
     url = globals.apiURL + endpoint
     response = json.loads(getFromAPI(url))
     return response
 
+#Loads summary trip info from Mojio, returns 2d array (list) with summary data by trip
+def getFilteredTrips(tripsJSON):
+    # print(tripsJSON)
+    allRelData = [] #list for final storage of all relevant data
+    for trip in tripsJSON["Data"]:
+        #Trip ID // Start City // End City // Start Timestamp // Distance[m] // Fuel Efficiency[km/L]// Vehicle ID
+        try:
+            relevantDataList = {"tid":              trip["Id"],
+                                "startCity":        trip["StartLocation"]["Address"]["City"],
+                                "endCity":          trip["EndLocation"]["Address"]["City"],
+                                "startTimestamp":   trip["StartTimestamp"],
+                                "distance":         trip["Distance"]["Value"],
+                                "efficiency":       trip["FuelEfficiency"]["Value"],
+                                "vid":              trip["VehicleId"]}
+            allRelData.append(relevantDataList)
+        except:
+            relevantDataList = {"tid": trip["Id"],
+                                "startCity": "...",
+                                "endCity": "...",
+                                "startTimestamp": trip["StartTimestamp"],
+                                "distance": trip["Distance"]["Value"],
+                                "efficiency": trip["FuelEfficiency"]["Value"],
+                                "vid": trip["VehicleId"]}
+            allRelData.append(relevantDataList)
+
+    return allRelData
+
+#unused at the moment
 def getEvents(tripID):
     url = globals.apiURL + 'trips/' + tripID + '/history/states?top=9999'
     response = json.loads(getFromAPI(url))
-    print(type(response))
+    # print(type(response))
     return response
 
-def init():
-    #do all the data loading here
-    return #stub
+def loadTripsIntoDB(userEmail):
+    unfilteredTrips = getUnfilteredTrips()
+    filteredTrips = getFilteredTrips(unfilteredTrips)
+    insertTuples(filteredTrips, userEmail)
+
+    return
+
+
+def insertTuples(filteredTrips, userEmail):
+    cur = globals.__conn.cursor()
+
+    #   tid   VARCHAR(64) PRIMARY KEY,
+    #   vid   VARCHAR(64) REFERENCES vehicle (vid),
+    #   email VARCHAR(64) REFERENCES human (email),
+    #   startCity VARCHAR(64),
+    #   endCity VARCHAR(64),
+    #   distance REAL,
+    #   efficiency REAL,
+    #   date TIMESTAMP
+
+    for trip in filteredTrips:
+        print( "%s, %s, %s, %s, %s, %s, %s, %s)",
+        (trip["tid"], trip["vid"], userEmail, trip["startCity"], trip["endCity"],
+         trip["distance"], trip["efficiency"], trip["startTimestamp"]))
+
+        cur.execute("INSERT INTO trip (tid, vid, email, startCity, endCity, distance, efficiency, date) "
+                    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                    (trip["tid"], trip["vid"], userEmail, trip["startCity"], trip["endCity"],
+                     trip["distance"], trip["efficiency"], trip["startTimestamp"]))
+
+        globals.__conn.commit()
+
+    return
