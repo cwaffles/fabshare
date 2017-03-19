d3b053784bb116599aefa9f856755cc6ffe7f98b
jorenj <joren.jackson@gmail.com>
2017-03-18 17:23:28 -0700

    Added/modified functions for getting and parsing json data. No functionality

--- rh.py
+++ rh.py
@@ -1,16 +1,29 @@
 import requests
+import json
 import subprocess
 
 #server refers to mojio's api
 apiURL = 'https://api.moj.io/v2/'
 authToken = 'Bearer 70c22344-e864-45e1-be1c-8b20db5cfb39'
 
+
+
+def getFromAPI(url):
+    headers = {'Authorization': authToken}
+    print(url)
+    return requests.get(url, headers=headers).content
+
 def getTrips():
     endpoint = 'trips'
-    headers = {'Authorization': authToken}
-    reqUrl = apiURL + endpoint
-    print(reqUrl)
-    return requests.get(reqUrl, headers=headers).content
+    url = apiURL + endpoint
+    response = json.loads(getFromAPI(url))
+    return response
+
+def getEvents(tripID):
+    url = apiURL + 'trips/' + tripID + '/history/states'
+    response = json.loads(getFromAPI(url))
+    return response
+
 
 
 
