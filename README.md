af60f30cb77b2b7217d639c8947bd9c274e1b53e
cwaffles <cwaffles@users.noreply.github.com>
2017-03-18 19:52:23 -0700

    add updated schema

--- README.md
+++ README.md
@@ -4,5 +4,11 @@ Required packages:
 -flask
 -ipdb
 -requests
+-psycopg2
 
-pip install flask ipdb requests
+pip install flask ipdb requests psycopg2
+
+How to launch:
+python launch.py
+
+Go to http://127.0.0.1:5000/
---
+++ schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE human (
+  email VARCHAR(64) PRIMARY KEY
+);
+
+CREATE TABLE vehicle (
+  vid   VARCHAR(64) PRIMARY KEY,
+  email VARCHAR(64) REFERENCES human (email)
+);
+
+CREATE TABLE trip (
+  tid   VARCHAR(64) PRIMARY KEY,
+  vid   VARCHAR(64) REFERENCES vehicle (vid),
+  email VARCHAR(64) REFERENCES human (email)
+);
+
+CREATE TABLE event (
+  etime TIMESTAMP PRIMARY KEY,
+  tid   VARCHAR(64) REFERENCES trip (tid)
+  --other EVENT params here
+);
+
+CREATE TABLE trip_participants (
+  tid   VARCHAR(64) REFERENCES trip (tid),
+  email VARCHAR(64) REFERENCES human (email),
+  PRIMARY KEY (tid, email)
+);
+
+CREATE TABLE vehicle_ownership (
+  email VARCHAR(64) REFERENCES human (email),
+  vid   VARCHAR(64) REFERENCES vehicle (vid),
+  PRIMARY KEY (email, vid)
+);
+
