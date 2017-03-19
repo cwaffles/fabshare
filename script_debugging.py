8166eccce15ca9ca0c20619ce240e840bcbb76fc
jorenj <joren.jackson@gmail.com>
2017-03-18 17:31:10 -0700

    Added/modified functions for getting and parsing json data. No functionality

---
+++ script_debugging.py
@@ -0,0 +1,24 @@
+import rh
+import ipdb
+from flask import Flask
+app = Flask(__name__)
+
+
+trips = rh.getTrips()
+
+rh.getEvents('dfa04b28-3882-49a9-8cb6-8fd6f6163885')
+
+
+
+# ipdb.set_trace() #for debug
+print(rh.nodePort)
+
+
+initialized = False
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
